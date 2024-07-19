2c4cffffa387fe4b2313100b883c748e0b2d4783
ArunJ03 <smilearun03@gmail.com>
2024-07-19 18:03:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TimerComponent from './Component/TimerComponent/TimerComponent'
+
+function App() {
+  return (
+    <React.Fragment>
+      <TimerComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
